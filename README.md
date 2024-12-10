9b8709559ad49158eb047374f85007581e0714fb
lucasf-nl <180476097+lucasf-nl@users.noreply.github.com>
2024-12-10 23:05:58 +0100

    Add Tailwind CSS and a Footer component

---
+++ README.md
@@ -0,0 +1,8 @@
+# CHE Semester 1 FDD
+
+A website produced during the first semester of HBO-ICT at the CHE.
+
+## Icon libraries used
+
+- Brand Icons: [Simple Icons](https://simpleicons.org)
+- Generic Icons: [Material Symbols](https://google.com)
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-    base: '/2425/lmfehres'
+  base: '/2425/lmfehres',
+  integrations: [tailwind(), icon()]
 });
