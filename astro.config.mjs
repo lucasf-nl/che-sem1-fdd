f6c8b46df82e6aad3939e6cb63c4aa9f9254dae2
lucasf-nl <180476097+lucasf-nl@users.noreply.github.com>
2024-12-12 15:16:48 +0100

    Add MDX and Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx'
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   base: '/2425/lmfehres',
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon(), mdx()],
   experimental: {
     svg: true
   }
